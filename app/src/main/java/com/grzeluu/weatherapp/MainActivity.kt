50e393237d730c6e4072bb62bde9b49fd0c9d1ed
Grzeluu <patryk.grzelewski6@gmail.com>
2021-12-23 21:03:10 +0100

    Location check added

--- app/src/main/java/com/grzeluu/weatherapp/MainActivity.kt
+++ app/src/main/java/com/grzeluu/weatherapp/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.grzeluu.weatherapp
 
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import com.grzeluu.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+
+        if (!isLocationEnabled()) {
+            showMessage("Your location provider is turned off. Please turn it on")
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+
+        } else {
+            showMessage("Your location provider is turned ON")
+        }
+    }
+
+    private fun showMessage(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 }
