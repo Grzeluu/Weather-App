bc87485d27493b300e873d9610b9933596118af3
Grzeluu <patryk.grzelewski6@gmail.com>
2022-01-03 23:34:46 +0100

    Add DiffUtilCallback

---
+++ app/src/main/java/com/grzeluu/weatherapp/ui/adapters/HourlyAdapter.kt
@@ -0,0 +1,21 @@
+package com.grzeluu.weatherapp.ui.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class HourlyAdapter : RecyclerView.Adapter<HourlyAdapter.HourlyViewHolder>() {
+    inner class HourlyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): HourlyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(p0: HourlyViewHolder, p1: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
