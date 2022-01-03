00a8380a857ee3a37c9b92507c8b5349746cfecf
Grzeluu <patryk.grzelewski6@gmail.com>
2022-01-03 12:36:10 +0100

    Add hourly forecast UI

--- app/src/main/java/com/grzeluu/weatherapp/network/RetrofitInstance.kt
+++ app/src/main/java/com/grzeluu/weatherapp/network/RetrofitInstance.kt
@@ -1,11 +1,14 @@
 package com.grzeluu.weatherapp.network
 
 import com.grzeluu.weatherapp.util.Constants
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
     companion object {
+
         private val retrofitWeather by lazy {
             Retrofit.Builder()
                 .baseUrl(ApiConstants.BASE_URL)
